649c56e125fcad21dfc42405be6e878ea61adece
Ryker Zhu <ryker.zhu@outlook.com>
2026-08-05 02:22:15 +0800

    refactor(odf): remove dead formula migration staging

--- crates/litchi-odf-formula/src/migration/legacy/mod.rs
+++
@@ -1,8 +0,0 @@
-//! OpenDocument Formula (`.odf` and `.otf`) support.
-
-pub mod builder;
-mod document;
-mod edit;
-mod serialize;
-
-pub use document::{FormulaDocument, MathAttribute, MathContent, MathElement, MathElementKind};
